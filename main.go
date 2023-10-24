e37f0d7efa7599ab007ed5aae014fd998534cb28
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-24 13:05:44 +0530

    connect cli with db

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "task/cmd"
+import (
+	"path/filepath"
+	"task/cmd"
+	"task/db"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	err := db.Init(dbPath)
+
+	if err != nil {
+		panic(err)
+	}
+
 	cmd.Execute()
 }
