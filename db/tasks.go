f4f4d9dfb15c1cd23a88f1c29086a2494f6426b1
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-24 12:48:21 +0530

    Add init block

--- db/tasks.go
+++ db/tasks.go
@@ -3,12 +3,25 @@ package main
 import (
 	"encoding/binary"
 	"task/models"
+	"time"
 
 	"github.com/boltdb/bolt"
 )
 
 var db *bolt.DB
 var taskBucket = []byte("tasks")
+
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
+}
 
 func ListTasks() ([]models.Task, error) {
 	var tasks []models.Task
